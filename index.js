472765afaa80bf07ea9f3c01f1a5158be5e2994e
Clemens <operations.sg65@gmail.com>
2024-01-23 16:46:42 +0800

    Assignment 4 done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+let principal, rate, time;
+
+principal = 2_000;
+rate = 0.2;
+time = 60;
+
+let totalAmount = principal * (1 + rate) * time;
+console.log(`The total amount is : ${totalAmount}`);
+
+const arrInvestments = [
+  [500, 0, 2, 24],
+  [750, 0.3, 18],
+  [1000, 0.2, 36],
+];
+
+let calculateInvestment = "";
+
+for (let x of arrInvestments) {
+  calculateInvestment += x[0] * (1 + x[1]) * x[2];
+}
+console.log(`The total investment caluculated is: ${calculateInvestment}`);
+
+let futureValues = arrInvestments.map(
+  (arrInvestment) =>
+    arrInvestment[0] * (1 + arrInvestment[1]) * arrInvestment[2]
+);
+console.log(`The future values are: ${futureValues}`);
+
+let aboveMin = futureValues.filter((futureValue) => futureValue > 20000);
+console.log(aboveMin);
